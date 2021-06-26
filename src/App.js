70284ffe03cfc9f88eb04c3aa75a7e2a38a2ad55
xoonziip <sanjivchaudhary416@gmail.com>
2021-06-26 14:46:38 +0545

    asdasd

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
 import Navbar from './components/Navbar/Navbar';
 import Homepage from './components/Homepage/Homepage'
-import Footer from './components/Footer/Footer'
+// import Footer from './components/Footer/Footer'
 
 function App() {
   return (
